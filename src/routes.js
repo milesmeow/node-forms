5c189cfc6f9203a7954bacf016aa9b2c695f6ade
James Tu <jamestu@JT-MBPro.local>
2019-08-19 16:37:37 -0700

    adds route for /contact and updated contact template

--- src/routes.js
+++ src/routes.js
@@ -6,4 +6,25 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
+router.get('/contact', (req, res) => {
+  res.render('contact', {
+    data: {},
+    errors: {}
+  })
+})
+
+router.post('/contact', (req, res) => {
+  res.render('contact', {
+    data: req.body, // { message, email }
+    errors: {
+      message: {
+        msg: 'A message is required'
+      },
+      email: {
+        msg: 'That email doesn‘t look right'
+      }
+    }
+  })
+})
+
 module.exports = router
