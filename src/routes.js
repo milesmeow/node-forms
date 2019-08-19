df16729780d7c7a3b66032fcb41db87bd6535720
James Tu <jamestu@JT-MBPro.local>
2019-08-19 16:43:51 -0700

    sanitize

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,8 @@ const {
   validationResult,
 } = require('express-validator/check')
 
+const { matchedData } = require('express-validator/filter')
+
 router.get('/', (req, res) => {
   res.render('index')
 })
@@ -21,16 +23,22 @@ router.get('/contact', (req, res) => {
 router.post('/contact', [
   check('message')
     .isLength({ min: 1 })
-    .withMessage('Message is required'),
+    .withMessage('Message is required')
+    .trim(),
   check('email')
     .isEmail()
     .withMessage('That email doesn‘t look right')
+    .trim()
+    .normalizeEail()
 ], (req, res) => {
   const errors = validationResult(req)
   res.render('contact', {
-    data: req.body,
+    data: req.body, //Should this be matchedData(req) ?
     errors: errors.mapped()
   })
+
+  const data = matchedData(req) // data is the sanitized input
+  console.log('Sanitized:', data)
 })
 
 module.exports = router
